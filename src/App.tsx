d7fb496ca7c51661db406076d594fd014161c8ed
quentin <quentinrichard97@gmail.com>
2021-12-24 17:09:47 +0100

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import {TopLayout} from "./layout/top/top";
 import {ContainerLayout} from "./layout/container/container";
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {ModalProvider} from "./components/modal/modal";
 
 function App() {
     return (
         <>
-            <Router>
+            <HashRouter>
                 <ModalProvider>
                     <TopLayout></TopLayout>
                     <ContainerLayout></ContainerLayout>
                 </ModalProvider>
-            </Router>
+            </HashRouter>
         </>
     );
 }
