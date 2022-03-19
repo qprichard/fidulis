36060169817c16880df83e36f89f254123e8614d
quentin <quentinrichard97@gmail.com>
2022-03-19 16:09:10 +0100

    update mentions

--- src/pages/mentions/mentions.tsx
+++ src/pages/mentions/mentions.tsx
@@ -2,13 +2,17 @@ import "./mentions.scss";
 
 export const MentionsPage = () => (
     <div className="mentions">
-        <div className="page-subtitle underline">MENTIONS LÉGALES</div>
         <p>
+            <div className="page-subtitle underline">MENTIONS LÉGALES</div>
             Site édité par Fidulis Avocat
             <br/>
             Directrice de la publication : SELARL Fidulis Avocat représentée par Mme Estelle Martinet
             <br/>
             Fidulis Avocat – SIREN 833665482 – 2, place Gailleton – 69002 Lyon
+            <br/>
+            Numéro de téléphone: <a href="tel:+33481926076">0481926076</a>
+            <br/>
+            Email: <a href="mailto:emartinet@fidulis.fr">emartinet@fidulis.fr</a>
             <br/>
             TVA Communautaire : FR62833665482
             <br/>
@@ -27,8 +31,6 @@ export const MentionsPage = () => (
         </p>
         <p>
             <div className="page-subtitle underline">HÉBERGEMENT</div>
-
-            <p>
                 Site hebergé par la société OVH SAS au capital de 500 000 €
                 <br/>
 
@@ -39,20 +41,16 @@ export const MentionsPage = () => (
                 <a href="https://www.ovh.com">www.ovh.com</a>
                 <br/>
                 Service client, composer le : 1007.
-            </p>
-
-            <p>
-                Pour l’exercice de vos droits d’accès de rectification ou de suppression de vos données personnelles faites
-                parvenir un mail à : <a href="mailto:cil@ovh.net">cil@ovh.net</a> ou compléter le <a href="https://www.ovh.com/fr/protection-donnees-personnelles/exercer-vos-droits/" className="form">formulaire OVH disponible en ligne</a> à cet effet.
-                <br/>
-                Pour le signalement d'abus et de contenus illicites, un <a href="https://www.ovh.com/abuse/#!/" className="form">formulaire de contact</a> est également disponible.
-                <br/>
-            </p>
+        </p>
+        <p>
+            Pour l’exercice de vos droits d’accès de rectification ou de suppression de vos données personnelles faites
+            parvenir un mail à : <a href="mailto:cil@ovh.net">cil@ovh.net</a> ou compléter le <a href="https://www.ovh.com/fr/protection-donnees-personnelles/exercer-vos-droits/" className="form">formulaire OVH disponible en ligne</a> à cet effet.
+            <br/>
+            Pour le signalement d'abus et de contenus illicites, un <a href="https://www.ovh.com/abuse/#!/" className="form">formulaire de contact</a> est également disponible.
+            <br/>
         </p>
         <p>
             <div className="page-subtitle underline">PROPRIÉTÉ INTELLECTUELLE</div>
-
-
             L’utilisateur s’engage à ne pas reproduire, résumer, modifier, altérer ou rediffuser, sans autorisation
             expresse préalable de l’éditeur, quelque texte, titre, application, logo, marque, information ou
             illustration, pour un usage autre que strictement privé, ce qui exclut toute représentation à des fins
@@ -61,7 +59,6 @@ export const MentionsPage = () => (
         </p>
         <p>
             <div className="page-subtitle underline">LE CONTENU DU SITE</div>
-
             Le site, les textes, les images, les sons, les photos, les graphiques, les fichiers téléchargeables et plus
             généralement tous les objets contenus dans le site sont la propriété exclusive de Fidulis Avocat et/ou des
             auteurs mentionnés. Ce site et les éléments qui le composent constituent des œuvres protégées par la
@@ -76,12 +73,9 @@ export const MentionsPage = () => (
 
         <p>
             <div className="page-subtitle underline">DONNÉES PERSONNELLES ET DROIT DE RECTIFICATION</div>
-
             Conformément à la loi n°78-17 du 6 janvier 1978, dite Loi Informatique et Liberté, vous disposez d’un droit
             d’accès, de modification, de rectification ou de suppression vous concernant en vous adressant à :
             <br/>
-
-
             Fidulis Avocat – emartinet@fidulis.fr – 2, place Gailleton – 69002 Lyon – <a href="tel:+33481926076">04 81 92 60 76</a>
         </p>
     </div>
